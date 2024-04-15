1bdb2f7f98946f96439a2bc4039ce9da33a15dec
Ruairi Moran <ruairi.moran@equipmentshare.com>
2024-04-15 16:42:09 +0100

    fit ci to this project

--- ci/README.md
+++ ci/README.md
@@ -1,8 +1,1 @@
-To run test script: `bash ./ci/script.sh`
-
-# Info:
-- Make sure you are using the correct python version (3.10): `python --version`
-- Problem: `ModuleNotFoundError: No module named 'tkinter'`
-    - Explanation: Your python installation does not include tkinter support
-    - Solution:
-        - Linux: `apt-get install python3-tk`
+Run test script from terminal: `bash ./ci/script.sh`
--- ci/script.sh
+++ ci/script.sh
@@ -1,37 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-test_all() {
-#    # Run Python tests
-#    # ------------------------------------
-#
-#    # -- create virtual environment
-#    export PYTHONPATH=.
-#
-#    # -- install virtualenv
-#    pip install virtualenv
-#
-#    # -- create virtualenv
-#    virtualenv -p python3.10 venv
-#
-#    # -- activate venv
-#    source venv/bin/activate
-#
-#    # -- upgrade pip within venv
-#    pip install --upgrade pip
-#
-#    # -- install dependencies
-#    pip install .
-#
-#    # -- run the python tests
-#    python -W ignore tests/test_tree_factories.py -v
-#
-#
-#    # Run C++ gtests using cmake
-#    # ------------------------------------
-#
-#    # -- generate simple tree data for testing
-#    python tests/test_tree_main.py
+tests() {
+    # Run CUDA/C++ gtests
+    # ------------------------------------
 
     # -- create build files
     cmake -S . -B ./build -Wno-dev
@@ -40,16 +12,16 @@ test_all() {
     cmake --build ./build
 
     # -- run tests
-    ctest --test-dir ./build/tests --output-on-failure
+    ctest --test-dir ./build/test --output-on-failure
 
     # -- run compute sanitizer
-    cd ./build/tests
-    /usr/local/cuda-12.3/bin/compute-sanitizer --tool memcheck --leak-check=full ./spock_tests
+    cd ./build/test
+    /usr/local/cuda-12.3/bin/compute-sanitizer --tool memcheck --leak-check=full ./device_test
 }
 
 
 main() {
-    test_all
+    tests
 }
 
 main
