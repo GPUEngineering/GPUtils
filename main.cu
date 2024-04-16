a925b17c1e4d3b1fe0d9c3ec72003a3f7153e01b
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-04-16 14:17:29 +0100

    Cholesky factorisation works, but... solve doesnt

--- main.cu
+++ main.cu
@@ -3,23 +3,24 @@
 #include <cublas_v2.h>
 #include "include/device.cuh"
 
-#define real_t float
+#define real_t double
 
 
 int main() {
     Context context;
 
-    size_t kk = 6;
-    std::vector<real_t> bbData{1.0, 2.0, 3.0,
-                               6.0, 7.0, 8.0,
-                               9.0, 10.0, 11.0,
-                               12.0, 13.0, 14.0,
-                               15.0, 16.0, 17.0,
-                               18.0, 19.0, 20.0};
-    DeviceMatrix<real_t> BB(context, kk, bbData, MatrixStorageMode::rowMajor);
-    auto copiedRows = BB.getRows(1, 4);
-    std::cout << copiedRows << "\n ---4";
+    size_t kk = 3;
+    std::vector<real_t> bbData{10.0, 2.0, 1.0,
+                               2.0, 70.0, 1.5,
+                               1.0, 1.5, 11.0};
+    DeviceMatrix<real_t> B(context, kk, bbData, MatrixStorageMode::rowMajor);
+    CholeskyFactoriser<real_t> choleskiser(context, B);
+    std::cout << "status = " << choleskiser.factorise() << std::endl;
+    std::cout << B;
 
-
+    std::vector<real_t> bData = {-1.0, -2.0, 10.0};
+    DeviceVector<real_t> b(context, bData);
+    std::cout << "status = " <<  choleskiser.solve(b) << std::endl << std::endl;
+    std::cout << b;
     return 0;
 }
