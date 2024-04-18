bb79369655928fd411b2e52cc0804e7cb3de1885
Ruairi Moran <ruairi.moran@equipmentshare.com>
2024-04-18 01:01:52 +0100

    refactor example to suit new file names

--- example/main.cu
+++ example/main.cu
@@ -1,10 +1,10 @@
-#include <device.cuh>
+#include <gputils.cuh>
 #include "src/data.cuh"
 
 
 int main() {
     auto d_c = d_A * d_b;  // Matrix-vector multiplication on your device
-    std::cout << d_c << "\n";  // Print result from your device
+    std::cout << d_c << "\n";  // Print result `DeviceVector [3]: [23, 53, 83]` from your device.
 
     return 0;
 }
--- example/src/data.cuh
+++ example/src/data.cuh
@@ -1,11 +1,10 @@
-#include <device.cuh>
+#include <gputils.cuh>
 
 
-Context context;  // Create only one context for the project!
 size_t rows = 3;  // Number of rows of matrix A
 std::vector<float> A = {1., 2.,
                         3., 4.,
                         5., 6.};  // Matrix A in row-major storage
 std::vector<float> b = {7., 8.};  // Vector b
-DeviceMatrix<float> d_A(context, rows, A, MatrixStorageMode::rowMajor);  // Matrix A living on your device
-DeviceVector<float> d_b(context, b);  // Vector b living on your device
+DeviceMatrix<float> d_A(rows, A, rowMajor);  // Matrix A living on your device (provided in row-major)
+DeviceVector<float> d_b(b);  // Vector b living on your device
