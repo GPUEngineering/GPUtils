e3a717298b0a2f86ac2c7a80762d254b6acc3628
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-04-19 19:53:13 +0100

    SVD and Nullspace
    About:
    - SVD: rank is tensor (tested)
    - Nullspace, draft impl. (WIP, untested)

--- main.cu
+++ main.cu
@@ -8,14 +8,13 @@
 
 
 int main() {
-    std::vector<real_t> aData{10.0, -2.0, -3.0,
-                              7.0, 20.0, -1.0,
-                              1.89, 60.0, -1.6,
-                              -4.5, 20.0, -1.1};
-    DTensor<real_t> A(aData, 3, 2, 2);
-    DTensor<real_t> At = A.tr();
-    std::cout << A;
-    std::cout << At;
+    std::vector<real_t> aData{1, 4, 2, 5, 3, 6,
+                              1, 4, 2, 5, 3, 7};
+    DTensor<real_t> A(aData, 2, 3, 2);
+
+//    std::cout << "A = " << A << " ------ \n";
+
+    Nullspace null(A);
 
     return 0;
 }
