621050895cd3655c2cc130f328aee040add164a3
Ruairi Moran <ruairi.moran@equipmentshare.com>
2024-04-19 15:34:17 +0100

    remove old files

--- main.cu
+++ main.cu
@@ -8,9 +8,9 @@
 
 int main() {
     std::vector<real_t> aData{10.0, -2.0, -3.0,
-                         7.0, 20.0, -1.0,
-                          1.89, 60.0, -1.6,
-                          -4.5, 20.0, -1.1};
+                              7.0, 20.0, -1.0,
+                              1.89, 60.0, -1.6,
+                              -4.5, 20.0, -1.1};
     DTensor<real_t> A(aData, 3, 2, 2);
     DTensor<real_t> At = A.tr();
     std::cout << A;
