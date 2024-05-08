bc0491541a48c31440c9f25390a69e20824e99f7
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-05-08 13:48:39 +0100

    memcheck only on a40

--- ci/script.sh
+++ ci/script.sh
@@ -30,34 +30,33 @@ tests() {
     # -- run tests
     ctest --test-dir ./build/test --output-on-failure
 
-    if [ ! -z "$(hwInfoOrin)" ]; then
-      return;
+    if [ -z "$(hwInfoOrin)" ]; then
+
+      # -- run compute sanitizer
+      cd ./build/test
+      mem=$(/usr/local/cuda/bin/compute-sanitizer --tool memcheck --leak-check=full ./device_test)
+      grep "0 errors" <<< "$mem"
+      cd ../..
+
+      # ------------------------------------
+      # Run example executable
+      # ------------------------------------
+
+      # -- create build files
+      cd example
+      cmake  -DCPPVERSION=${cpp_version} -DSM_ARCH=${sm_arch} -S . -B ./build -Wno-dev
+
+      # -- build files in build folder
+      cmake --build ./build
+
+      # -- run main.cu
+      ./build/example_main
+
+      # -- run compute sanitizer
+      cd ./build
+      mem=$(/usr/local/cuda/bin/compute-sanitizer --tool memcheck --leak-check=full ./example_main)
+      grep "0 errors" <<< "$mem"
     fi
-
-    # -- run compute sanitizer
-    cd ./build/test
-    mem=$(/usr/local/cuda/bin/compute-sanitizer --tool memcheck --leak-check=full ./device_test)
-    grep "0 errors" <<< "$mem"
-    cd ../..
-
-    # ------------------------------------
-    # Run example executable
-    # ------------------------------------
-
-    # -- create build files
-    cd example
-    cmake  -DCPPVERSION=${cpp_version} -DSM_ARCH=${sm_arch} -S . -B ./build -Wno-dev
-
-    # -- build files in build folder
-    cmake --build ./build
-
-    # -- run main.cu
-    ./build/example_main
-
-    # -- run compute sanitizer
-    cd ./build
-    mem=$(/usr/local/cuda/bin/compute-sanitizer --tool memcheck --leak-check=full ./example_main)
-    grep "0 errors" <<< "$mem"
 }
 
 
