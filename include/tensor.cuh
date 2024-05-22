546f54bb99cfeaca92e0f765f597cfbe1e59624b
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-05-22 12:56:11 +0100

    fix typo

--- include/tensor.cuh
+++ include/tensor.cuh
@@ -33,11 +33,11 @@
  * and number of threads per block
  *
  * @param n problem size
- * @param threads_pre_block threads per block (defaults to THREADS_PER_BLOCK)
+ * @param threads_per_block threads per block (defaults to THREADS_PER_BLOCK)
  * @return number of blocks
  */
-constexpr size_t numBlocks(size_t n, size_t threads_pre_block=THREADS_PER_BLOCK) {
-    return (n / threads_pre_block + (n % threads_pre_block != 0));
+constexpr size_t numBlocks(size_t n, size_t threads_per_block=THREADS_PER_BLOCK) {
+    return (n / threads_per_block + (n % threads_per_block != 0));
 }
 
 
