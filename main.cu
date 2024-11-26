a0ff61132eb022a4f0930980c8e19f8492a7a1c3
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-11-26 18:48:40 +0000

    experimenting with DTensor construction from file
    - Simple text-based serialisation of tensor data
    - example in main.cu where we read data from file

--- main.cu
+++ main.cu
@@ -1,27 +1,57 @@
-#include <random>
-#include <algorithm>
-#include <iterator>
-#include <vector>
-#include <iostream>
-#include <cublas_v2.h>
 #include "include/tensor.cuh"
-#include <memory>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-#define real_t double
+template<typename T>
+struct data_t {
+    size_t numRows;
+    size_t numCols;
+    size_t numMats;
+    std::vector<T> data;
+};
+
+template<typename T>
+data_t<T> vectorFromFile(std::string path_to_file) {
+    data_t<T> dataStruct;
+    std::ifstream file;
+    file.open(path_to_file, std::ios::in);
+
+    std::string line;
+    getline(file, line); dataStruct.numRows = atoi(line.c_str());
+    getline(file, line); dataStruct.numCols = atoi(line.c_str());
+    getline(file, line); dataStruct.numMats = atoi(line.c_str());
+
+    size_t numElements = dataStruct.numRows * dataStruct.numCols * dataStruct.numMats;
+    std::vector<T> vecDataFromFile(numElements);
+
+    size_t i = 0;
+    while (getline(file, line)) {
+        if constexpr (std::is_same_v<T, int>) {
+            vecDataFromFile[i] = atoi(line.c_str());
+        } else if constexpr (std::is_same_v<T, double>) {
+            vecDataFromFile[i] = std::stod(line.c_str());
+        } else if constexpr (std::is_same_v<T, float>) {
+            vecDataFromFile[i] = std::stof(line.c_str());
+        }
+        if (i == numElements - 1) break;
+        i++;
+    }
+    file.close();
+
+    dataStruct.data = vecDataFromFile;
+    return dataStruct;
+}
 
 int main() {
+    auto z = vectorFromFile<double>("../test/data/my.dtensor");
+    for (size_t i = 0; i < 3; i++) std::cout << z.data[i] << ", ";
+    DTensor<double> dz(z.data, z.numRows, z.numCols, z.numMats);
+    std::cout << "\n\n";
 
-    std::vector<real_t> aData = {10.0, 2.0, 3.0,
-                            2.0, 20.0, -1.0,
-                            3.0, -1.0, 30.0};
-    DTensor<real_t> A(3, 3, 2);
-    DTensor<real_t> A0(A, 2, 0, 0);
-    DTensor<real_t> A1(A, 2, 1, 1);
-    A0.upload(aData);
-    A1.upload(aData);
-    CholeskyBatchFactoriser<real_t> chol(A);
-    chol.factorise();
-    std::cout << chol.info()(0);
+    auto q = vectorFromFile<int>("../test/data/my.dtensor");
+    for (size_t i = 0; i < 3; i++) std::cout << q.data[i] << ", ";
 
     return 0;
 }
