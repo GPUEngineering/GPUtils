279159780fd5c7b5acb0e445eff1aef8d913d580
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-11-26 19:08:57 +0000

    implement DTensor::parseFromFile

--- include/tensor.cuh
+++ include/tensor.cuh
@@ -11,6 +11,7 @@
 #include <memory>
 #include <optional>
 #include <cassert>
+#include <fstream>
 
 #ifndef TENSOR_CUH
 #define TENSOR_CUH
@@ -249,6 +250,8 @@ public:
      * @throws std::invalid_argument if T is other than double, float, or int
      */
     static DTensor<T> createRandomTensor(size_t numRows, size_t numCols, size_t numMats, T low, T hi);
+
+    static DTensor<T> parseFromTextFile(std::string path_to_file);
 
     /**
     * Constructs a DTensor object.
@@ -562,6 +565,54 @@ DTensor<T> DTensor<T>::createRandomTensor(size_t numRows, size_t numCols, size_t
         return a;
     }
     throw std::invalid_argument("[createRandomTensor] unsupported type T");
+}
+
+
+template<typename T>
+struct data_t {
+    size_t numRows;
+    size_t numCols;
+    size_t numMats;
+    std::vector<T> data;
+};
+
+template<typename T>
+data_t<T> vectorFromFile(std::string path_to_file) {
+    data_t<T> dataStruct;
+    std::ifstream file;
+    file.open(path_to_file, std::ios::in);
+    if (!file.is_open()) { throw std::exception(); };
+
+    std::string line;
+    getline(file, line); dataStruct.numRows = atoi(line.c_str());
+    getline(file, line); dataStruct.numCols = atoi(line.c_str());
+    getline(file, line); dataStruct.numMats = atoi(line.c_str());
+
+    size_t numElements = dataStruct.numRows * dataStruct.numCols * dataStruct.numMats;
+    std::vector<T> vecDataFromFile(numElements);
+
+    size_t i = 0;
+    while (getline(file, line)) {
+        if constexpr (std::is_same_v<T, int>) {
+            vecDataFromFile[i] = atoi(line.c_str());
+        } else if constexpr (std::is_same_v<T, double>) {
+            vecDataFromFile[i] = std::stod(line.c_str());
+        } else if constexpr (std::is_same_v<T, float>) {
+            vecDataFromFile[i] = std::stof(line.c_str());
+        }
+        if (i == numElements - 1) break;
+        i++;
+    }
+
+    dataStruct.data = vecDataFromFile;
+    file.close();
+    return dataStruct;
+}
+
+template<typename T> DTensor<T> DTensor<T>::parseFromTextFile(std::string path_to_file) {
+    auto parsedData = vectorFromFile<T>(path_to_file);
+    DTensor<T> tensorFromData(parsedData.data, parsedData.numRows, parsedData.numCols, parsedData.numMats);
+    return tensorFromData;
 }
 
 template<typename T>
--- main.cu
+++ main.cu
@@ -4,54 +4,9 @@
 #include <string>
 #include <vector>
 
-template<typename T>
-struct data_t {
-    size_t numRows;
-    size_t numCols;
-    size_t numMats;
-    std::vector<T> data;
-};
-
-template<typename T>
-data_t<T> vectorFromFile(std::string path_to_file) {
-    data_t<T> dataStruct;
-    std::ifstream file;
-    file.open(path_to_file, std::ios::in);
-
-    std::string line;
-    getline(file, line); dataStruct.numRows = atoi(line.c_str());
-    getline(file, line); dataStruct.numCols = atoi(line.c_str());
-    getline(file, line); dataStruct.numMats = atoi(line.c_str());
-
-    size_t numElements = dataStruct.numRows * dataStruct.numCols * dataStruct.numMats;
-    std::vector<T> vecDataFromFile(numElements);
-
-    size_t i = 0;
-    while (getline(file, line)) {
-        if constexpr (std::is_same_v<T, int>) {
-            vecDataFromFile[i] = atoi(line.c_str());
-        } else if constexpr (std::is_same_v<T, double>) {
-            vecDataFromFile[i] = std::stod(line.c_str());
-        } else if constexpr (std::is_same_v<T, float>) {
-            vecDataFromFile[i] = std::stof(line.c_str());
-        }
-        if (i == numElements - 1) break;
-        i++;
-    }
-    file.close();
-
-    dataStruct.data = vecDataFromFile;
-    return dataStruct;
-}
 
 int main() {
-    auto z = vectorFromFile<double>("../test/data/my.dtensor");
-    for (size_t i = 0; i < 3; i++) std::cout << z.data[i] << ", ";
-    DTensor<double> dz(z.data, z.numRows, z.numCols, z.numMats);
-    std::cout << "\n\n";
-
-    auto q = vectorFromFile<int>("../test/data/my.dtensor");
-    for (size_t i = 0; i < 3; i++) std::cout << q.data[i] << ", ";
-
+    auto z = DTensor<double>::parseFromTextFile("../test/data/my.dtensor");
+    std::cout << z;
     return 0;
 }
