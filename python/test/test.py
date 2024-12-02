94f7305032ffcd5ad05feb2f577f13a0394813a5
Pantelis Sopasakis <p.sopasakis@gmail.com>
2024-12-02 23:49:52 +0000

    test: set up class

--- python/test/test.py
+++ python/test/test.py
@@ -1,11 +1,22 @@
+import os
 import unittest
+import numpy as np
+import gputils_api as gpuapi
 
 
 class GputilApiTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        n = 5
+        eye_d = np.eye(n, dtype=np.dtype('d'))
+        gpuapi.write_array_to_gputils_binary_file(eye_d, 'eye_d.bt')
+        eye_f = np.eye(n, dtype=np.dtype('f'))
+        gpuapi.write_array_to_gputils_binary_file(eye_f, 'eye_f.bt')
+        xd = np.random.randn(2, 4, 6).astype('d')
+        gpuapi.write_array_to_gputils_binary_file(xd, 'rand_246_d.bt')
+        xd = np.random.randn(3, 5, 7).astype('f')
+        gpuapi.write_array_to_gputils_binary_file(xd, 'rand_357_f.bt')
 
     def test_asdf(self):
         pass
